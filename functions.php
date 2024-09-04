11cf74d6874e607403a316a35f2eaa62a966e063
KingEdward <edwardziadeh@gmail.com>
2024-09-04 16:01:31 +0300

    exclude woo_seo and add variable

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ include_once('functions/woo/image_attributes.php');
 include_once('functions/woo/import_product.php');
 include_once('functions/woo/products_vendor.php');
 include_once('functions/woo/woo_cat.php');
-include_once('functions/woo/woo_seo.php');
+//include_once('functions/woo/woo_seo.php');
 
 include_once('functions/tracking/visitor_detail.php');
 include_once('functions/tracking/mixpanel_head.php'); // only use in production
@@ -579,3 +579,29 @@ function unregister_klb_product_categories_widget() {
 }
 add_action('widgets_init', 'unregister_klb_product_categories_widget', 11);
 
+
+/**
+ * By: Edward Ziadeh
+ * To add new variable inside yoast
+ * Date: Sep 4, 2024
+ */
+function my_custom_yoast_variable() {
+    return 'edward the king';
+    // Example: Retrieving a custom field from a post, you can customize this as needed
+    $custom_value = get_post_meta(get_the_ID(), 'custom_field_key', true);
+    
+    // Return the value you want to display in Yoast SEO fields
+    return $custom_value ? $custom_value : 'Default Value';
+}
+
+// Step 2: Register the custom variable in Yoast SEO
+add_action('wpseo_register_extra_replacements', 'my_custom_yoast_variable_replacement');
+
+function my_custom_yoast_variable_replacement() {
+    wpseo_register_var_replacement(
+        '%%edward%%',  // This is the placeholder that will be used in Yoast settings
+        'my_custom_yoast_variable', // Callback function to retrieve the value
+        'advanced', // Usage context (use 'advanced' for custom variables)
+        'My Custom Variable Description' // Optional description for the variable
+    );
+}
