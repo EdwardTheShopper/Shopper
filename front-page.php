e6430621cdb9ba0c8de9df517d12a1b527d05889
KingEdward <edwardziadeh@gmail.com>
2024-09-10 12:39:48 +0300

    Fron page css

--- front-page.php
+++ front-page.php
@@ -3,17 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Shopper</title>
     <style>
         html {
-            margin-top: 0 !important;
+            margin-top: 0;
         }
-
-        /* hide irrelevant elements including header and parts of footer */
-        .home>div,
-        .home #masthead,
-        .home footer nav {
-            display: none !important;
+        body{
+            margin: 0;
         }
 
         /* more adjustments for footer */
@@ -28,8 +24,12 @@
         .home .site-footer .footer-bottom .container .site-copyright {
             height: inherit;
         }
-
+        .home .site-footer .footer-bottom
+        {
+            color: #9b9bb4;
+        }
         .home .site-footer .footer-bottom .container {
+            display: flex;
             margin: 0;
             max-width: 100%;
             padding: 0 !important;
@@ -68,10 +68,6 @@
     <main id="main" class="site-primary">
         <div class="site-content">
             <div class="homepage-content">
-
-
-
-
                 <div data-elementor-type="wp-page" data-elementor-id="50" class="elementor elementor-50">
                     <section class="elementor-section elementor-top-section elementor-element elementor-element-5a99381 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="5a99381" data-element_type="section">
                         <div class="elementor-container elementor-column-gap-default">
@@ -79,7 +75,7 @@
                                 <div class="elementor-widget-wrap elementor-element-populated">
                                     <div class="elementor-element elementor-element-77ed29c elementor-widget__width-inherit elementor-absolute elementor-widget-mobile__width-initial elementor-widget elementor-widget-html" data-id="77ed29c" data-element_type="widget" data-settings="{&quot;_position&quot;:&quot;absolute&quot;}" data-widget_type="html.default">
                                         <div class="elementor-widget-container">
-                                            <iframe src="https://www.storydoc.com/1c9312feaaeea0feacc7881c5bb0cd3f/fe61d7c-7b4-c824-ad65-17bc4c80d276/66d9eeb5ae7813298104cdc4" title="Shopper.shop" allow="clipboard-write; web-share; allow-scripts; allow-popups; allow-forms; allow-pointer-lock;" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>
+                                            <iframe src="https://www.storydoc.com/1c9312feaaeea0feacc7881c5bb0cd3f/fe61d7c-7b4-c824-ad65-17bc4c80d276/66d9eeb5ae7813298104cdc4" title="Shopper.shop" allow="clipboard-write; web-share; allow-scripts; allow-popups; allow-forms; allow-pointer-lock;" style="border-top:0; border-right:0; border-left:0;" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>
                                         </div>
                                     </div>
                                 </div>
@@ -89,8 +85,16 @@
                 </div>
             </div><!-- homepage-content -->
         </div><!-- site-content -->
-        
     </main>
+    <footer class="site-footer">
+        <div class="footer-bottom border-enable">
+            <div class="container">
+                <div class="site-copyright">
+                 <p style="font-size: 16px;">© כל הזכויות שמורות 2024 - TNG Shopper</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
